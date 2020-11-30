09b7c023b6547203f97e1e3d0347693979bd9a0d
eugeneburdukov <74274237+eugeneburdukov@users.noreply.github.com>
2020-11-30 19:25:37 +0200

    задачи с плюсом

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,17 +11,27 @@ public class Main {
         процент отрицательных чисел и процент нулей. Вычислить процент чётных чисел и процент нечётных.
          */
 
+        
+
 
 
         /*
         Вывести на экран таблицу умножения (таблицу Пифагора).
          */
 
-        for (int i = 2; i < 10; i++) {
-            for (int j = 2; j < 10; j++) {
-                System.out.println(i + " x " + j + " = " + i * j);
+        for (int i = 1; i < 10; i++) {
+            System.out.print(i + " ");
+            for (int j = 1; j < 10; j++) {
+                System.out.print(i * j + " ");
             }
+            System.out.println();
         }
+
+//        for (int i = 2; i < 10; i++) {
+//            for (int j = 2; j < 10; j++) {
+//                System.out.println(i + " x " + j + " = " + i * j);
+//            }
+//        }
 
         /*
 Палиндром - цифры.
