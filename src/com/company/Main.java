aa861c95560d3fd03ec708360cbda2796485d0b2
eugeneburdukov <74274237+eugeneburdukov@users.noreply.github.com>
2020-12-13 19:27:57 +0200

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,35 +25,34 @@ public class Main {
 
         int[] arr1 = new int[n];
 
-        System.out.print("Исходный массив, чья длина = " +  n + " : ");
+        System.out.print("Исходный массив, чья длина = " + n + " : ");
 
         for (int i = 0; i < arr1.length; i++) {
             arr1[i] = random.nextInt(n + 1);
             System.out.print(arr1[i] + " ");
-            for (int j = 0; j < arr1.length; j++) {
-                if (arr1[i] % 2 == 0 && arr1[i] != 0) {
-                    count++;
-                    break;
-                }
+            if (arr1[i] % 2 == 0 && arr1[i] != 0) {
+                count++;
             }
         }
 
         System.out.println();
 
-        int[] arr2 = new int[count];
+        if (count > 0) {
+            int[] arr2 = new int[count];
 
-        System.out.print("Второй массив только из чётных элементов первого массива : ");
+            System.out.print("Второй массив только из чётных элементов первого массива : ");
 
-        for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i] % 2 == 0 && arr1[i] != 0) {
-                for (int j = 0; j < arr2.length; j++) {
-                    arr2[j] = arr1[i];
-                    System.out.print(arr2[j] + " ");
-                    break;
+            for (int i = 0; i < arr1.length; i++) {
+                if (arr1[i] % 2 == 0 && arr1[i] != 0) {
+                    for (int j = 0; j < arr2.length; j++) {
+                        arr2[j] = arr1[i];
+                        System.out.print(arr2[j] + " ");
+                        break;
+                    }
                 }
             }
         }
-
+        
         /*
         изначальное решение
          */
